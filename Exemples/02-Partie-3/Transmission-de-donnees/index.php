1221eecf7099674d9cc3d2733d20007cb71be36a
kyweez <auboudier.pro@gmail.com>
2020-03-31 19:39:45 +0200

    Envoi de parametres par l'URL

---
+++ Exemples/02-Partie-3/Transmission-de-donnees/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transmissions de donnees avec URL</title>
+</head>
+
+<body>
+    <p>
+        <!--Transmet des arguments key/value a l'URL de href-->
+        <a href="bonjour.php?nom=Boudier&amp;prenom=Aurélien&amp;repeter=1">Dis-moi Bonjour</a>
+    </p>
+</body>
+
+</html>
