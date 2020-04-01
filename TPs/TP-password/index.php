e72464e937c309effce56949f5e0d4637ad0ecef
kyweez <auboudier.pro@gmail.com>
2020-04-01 23:19:07 +0200

    Finalisation (originale) du TP

--- TPs/TP-password/index.php
+++ TPs/TP-password/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- INFORMATION POUR TESTER : LE PASSWORD = Kangourou-->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- TPs/TP-password/secrets.php
+++ TPs/TP-password/secrets.php
@@ -11,26 +11,27 @@
 
 <body>
     <?php
+    $valid_password = 'Kangourou';
     function displayAccesDeniedHeader()
     {
         echo    '<header><h1>NASA Secrets</h1></header>';
     }
     function displayAccesGrantedHeader()
     {
-        echo    '<header><h1>Welcome ' . $_POST['name'] . '</h1></header>';
+        echo    '<header><h1>Welcome ' . htmlspecialchars($_POST['name']) . '</h1></header>';
     }
     function displayAccesDeniedMain()
     {
         echo    '<main>';
         echo    '<img style="
-                    padding-bottom: 10%;
-                    transform: rotate(0.04turn); 
-                    height: 45%; 
-                    width: 25%; 
-                    filter: contrast(1%);" 
-                    src="./img/ACCES_DENIED.png" 
-                    alt="DENIED"
-                >';
+                        padding-bottom: 10%;
+                        transform: rotate(0.04turn); 
+                        height: 45%; 
+                        width: 25%; 
+                        filter: contrast(1%);" 
+                        src="./img/ACCES_DENIED.png" 
+                        alt="DENIED"
+                    >';
         echo    '<p style="height: 20%; font-size: 1.5vw;" >This is a restricted access !</p>';
         echo    '<a style="height: 20%; text-decoration: none;"href="index.php">Click here to go back to the main page</a>';
         echo    '</main>';
@@ -39,26 +40,43 @@
     {
         echo    '<main>';
         echo    '<ul style="height: 75%; font-size:2vw; list-style: none;">
-                    <li>We can turn an asteroid into a spaceship</li>
-                    <li>Microbes can recycle PCBs on Mars</li>
-                    <li>A chariot with veil is on Venus</li>
-                    <li>Submarine sits in Titan’s oil oceans</li>
-                    <li>Sleeping in a space journey can be done during a long time</li>
-                    <li>We have a lunar concrete base</li>
-                    <li>A balloon will let us fly over Titan</li>
-                    <li>We are creating a robot "superball" which bounces on other planets</li>
-                    <li>We are developing microbes to "terraform" Mars</li>
-                    <li>We create a space builder spider</li>
-                </ul>';
+                        <li>We can turn an asteroid into a spaceship</li>
+                        <li>Microbes can recycle PCBs on Mars</li>
+                        <li>A chariot with veil is on Venus</li>
+                        <li>Submarine sits in Titan’s oil oceans</li>
+                        <li>Sleeping in a space journey can be done during a long time</li>
+                        <li>We have a lunar concrete base</li>
+                        <li>A balloon will let us fly over Titan</li>
+                        <li>We are creating a robot "superball" which bounces on other planets</li>
+                        <li>We are developing microbes to "terraform" Mars</li>
+                        <li>We create a space builder spider</li>
+                    </ul>';
         echo    '<a style="height: 10%; text-decoration: none;"href="index.php">Click here to go back to the main page</a>';
         echo    '</main>';
     }
-
-    //Form verification and page generation
-    if (isset($_POST['name']) and isset($_POST['password']))
+    function accessGranted()
     {
         displayAccesGrantedHeader();
         displayAccesGrantedMain();
+    }
+    function accessDenied()
+    {
+        displayAccesDeniedHeader();
+        displayAccesDeniedMain();
+    }
+    //Form verification and page generation
+    if (isset($_POST['name']) and isset($_POST['password'])) {
+        if (strlen($_POST['name']) != 0 and strlen($_POST['password']) != 0) 
+        {
+            strcmp($_POST['password'], $valid_password) == 0 ? accessGranted(): accessDenied(); 
+        }
+        else
+        {
+            //Insertion of a windows.alert en JS
+            echo '<script>alert("Please fill out the entire form.");</script>';
+            //Reload the index.php page
+            echo '<script>location.assign("index.php");</script>';
+        }
     } else {
         displayAccesDeniedHeader();
         displayAccesDeniedMain();
@@ -70,3 +88,12 @@
 </body>
 
 </html>
+
+<!--
+window.location (JS)
+    Renvoie un objet Location contenant des informations concernant l'URL 
+    actuelle du document et fournit des méthodes pour modifier cette URL.
+    Cette propriété peut être utilisée pour charger une autre page.
+    
+    assign(url)	= Charge le document situé à l'URL spécifiée.
+-->
