c702e0ebcf73812233fd3a5ee1c10a7b5be1b2e6
kyweez <auboudier.pro@gmail.com>
2020-04-01 13:18:24 +0200

    Front-end TP openclassrooms fini

--- TPs/TP-password/index.php
+++ TPs/TP-password/index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TP Password</title>
-    <link href='http://fonts.googleapis.com/css?family=Ubuntu:bold' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style_index.css">
 </head>
 
 <body>
@@ -14,13 +14,23 @@
         <h1>NASA Secrets</h1>
     </header>
     <main>
-        <p>
-            Hello, if you want to access to all the biggest NASA secrets, try to find the password !<br>
+        <p class="message">
+            Hello, if you want to access to all the biggest NASA secrets, try to find the password !
+        </p>
+        <p class="message">
             Fill the form behind :
         </p>
         <form action="secrets.php" method="POST">
-            <label for="name">Name : <input id="name" name="name" type="text" placeholder="Please insert your name"></label>
-            <label for="password">Password : <input id="password" name="password" type="password" placeholder="Please insert your password"></label>
+            <p>
+                <label for="name">Name :</label>
+                <input id="name" name="name" type="text" placeholder="Please insert your name"></p>
+            <p>
+                <label for="password">Password :</label>
+                <input id="password" name="password" type="password" placeholder="Please insert your password">
+            </p>
+            <p>
+                <input id="submit" type="submit" value="OK">
+            </p>
         </form>
     </main>
     <footer>
---
+++ TPs/TP-password/secrets.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP Password</title>
+    <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style_secrets.css">
+</head>
+
+<body>
+    <header>
+        <h1>NASA Secrets</h1>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <p>Copyright &copy; - Kyweez</p>
+    </footer>
+</body>
+
+</html>
